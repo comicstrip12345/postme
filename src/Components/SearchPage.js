e92103f66a8310193d3fd1aa1a66dd1d6fd7e9d9
comicstrip12345 <sabanalers@gmail.com>
2022-05-18 14:10:22 +0800

    Add design to Search Page

--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import NavbarNotLoggedIn from './NavbarNotLoggedIn'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { Link } from 'react-router-dom';
+import { Fade } from 'react-reveal';
 
 const SearchPage = () => {
 
@@ -15,78 +19,51 @@ const SearchPage = () => {
             console.log(response);
 
             if(response.status===200){
-        
                 setProfiles(response["data"]["result"])
-             
             } 
-
         })
         console.log(`${search}`)
     }
 
-  return (
-    <div className='container p-5'>
-        <h1 className='h1 text-center'>Search</h1>
-
-            <div className='row justify-content-center pt-5'>
-                <div className="col-4 form-floating mb-3 form">
-                    <input type="text" className="form-control" id="floatingFirstName" placeholder="name" onChange={(e)=>{setSearch(e.target.value)}} />
-
-                    <label htmlFor="floatingFirstName">Search your friend</label>
-                </div>
-
-                
-            </div>
-
-            <div className='submit pb-5'>
-                <div className='row justify-content-center'>
-                    <div className='col-3'>
-                        <input type="submit" onClick={searchHandler}/>
+    return (
+        <>
+            <NavbarNotLoggedIn/>
+            <section className='searchPage'>
+                <div className='container'>
+                    <div className='row pt-5'>
+                        <div className="col-12">
+                            <div className="form-floating mb-3 form">
+                                <button onClick={searchHandler}><i className="bi bi-search"></i></button>
+                                <input type="text" className="form-control" id="searchPage" placeholder="name" onChange={(e)=>{setSearch(e.target.value)}} />
+                                <label htmlFor="searchPage">Search your friend</label>
+                            </div>
+                            <div className='col-6'>
+                                {profiles.map((profile,index)=> (
+                                        <Fade>
+                                            <div className='row' key={index}>
+                                                <div className='col-2 image'>
+                                                    {/* container ng image sa future */}
+                                                    <img src="https://e7.pngegg.com/pngimages/505/761/png-clipart-login-computer-icons-avatar-icon-monochrome-black-thumbnail.png" alt="avatar"/>
+                                                </div>
+                                                <div className='col-10 detail d-flex align-items-center'>
+                                                    <h1>
+                                                        <Link to={`/profile/${profile.userid}`}>{profile.firstName} {profile.lastName}</Link> <br/>
+                                                        <small className='text-muted'>from {profile.city} </small>
+                                                    </h1>
+                                                </div>
+                                            </div>
+                                        </Fade>
+                                    ))
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <h1 className='h1 text-center'>Result</h1>
-
-                {
-                    profiles.map((profile,index)=> (
-                        <div className='container' key={index}>
-                            <div className='row justify-content-center'>
-                                <div className='col-3'>
-                                    {/* container ng image sa future */}
-                                    <img src="https://e7.pngegg.com/pngimages/505/761/png-clipart-login-computer-icons-avatar-icon-monochrome-black-thumbnail.png" alt="avatar" style={{width:"50px"}}></img>
-                                </div>
-
-                                <div className='col-9'>
-                                    <div className='row text-start'>     
-                                        {profile.firstName} {profile.lastName} 
-                                    </div>
-                                    <div className='row text-start'>     
-                                        <small className='text-muted'>from {profile.city} </small>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-
-                           
-
-                        </div>
-                    )
-
-                       
-
-                    )
-                }
-               
-
-
-
-
-
-    </div>
-
-
-  )
+            </section>
+            
+        </>
+        
+    )
 }
 
 export default SearchPage
